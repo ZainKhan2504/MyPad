0a079ce6ddf3d06290ee0ceced5ee09abd17a5a1
Zain Ali Khan <33342998+ZainKhan2504@users.noreply.github.com>
2018-09-20 16:09:25 +0500

    handled auth

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require("mongoose");
+const passport = require("passport");
 const app = express();
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Loads Routes
+const auth = require("./routes/auth");
 
 // Index route
 app.get("/", (req, res) => {
     res.send("Index");
 });
+
+// Use Routes
+app.use("/auth", auth);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
