fb210b2252e5088c7adf41add92f2abf8b73324d
Zain Ali Khan <33342998+ZainKhan2504@users.noreply.github.com>
2018-09-20 18:03:50 +0500

    Added google authentication

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
-const express = require('express');
-const mongoose = require("mongoose");
-const passport = require("passport");
-const app = express();
+const express      = require('express');
+const mongoose     = require("mongoose");
+const cookieParser = require("cookie-parser");
+const session      = require("express-session");
+const passport     = require("passport");
+const app          = express();
+
+// Loads Models
+require("./models/User");
 
 // Passport Config
 require("./config/passport")(passport);
 
 // Loads Routes
 const auth = require("./routes/auth");
+
+// Loads Keys
+const keys = require("./config/keys");
+
+// Map Global Promises
+mongoose.Promise = global.Promise; 
+
+// Mongoose Connect
+mongoose.connect(
+  keys.mongoURI,
+  {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Cookie Parser Middleware
+app.use(cookieParser());
+
+// Express Session Middleware
+app.use(session({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set Global Variables
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
 
 // Index route
 app.get("/", (req, res) => {
--- routes/auth.js
+++ routes/auth.js
@@ -9,4 +9,19 @@ router.get('/google/callback',
         res.redirect('/dashboard');
     });
 
+// Verify User Route
+router.get("/verify", (req, res) => {
+    if (req.user) {
+        console.log(req.user);
+    } else {
+        console.log("Not Auth");
+    }
+});
+
+// Logout Route
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
 module.exports = router;
